289aaf046d911323de918a56ab54f74c5e52548a
alex.luelmo <alex.luelmo@students.salle.url.edu>
2023-01-19 17:27:04 +0100

    Search page v1.0

--- lib/pages/Search.dart
+++ lib/pages/Search.dart
@@ -19,14 +19,14 @@ class _SearchState extends State<Search> {
   RecommendedMovies? searchedMovies;
   TextEditingController searchController = TextEditingController();
   final user = FirebaseAuth.instance.currentUser;
-  bool _isLiked = false;
+  final Map<int,bool> _moviesFavStatus = {};
 
   @override
   void initState() {
     super.initState();
   }
 
-  getUpcomingMovies(String query) async {
+  Future<void> getSearchedMovies(String query) async {
     var client = http.Client();
     var url = Uri.parse('https://api.themoviedb.org/3/search/movie?api_key=b5f80d427803f2753428de379acc4337&language=en-US&query=$query&page=1&include_adult=false');
     var response = await client.get(url);
@@ -34,9 +34,15 @@ class _SearchState extends State<Search> {
     if (response.statusCode == 200) {
       var data = response.body;
 
-      Map<String, dynamic> upcomingMoviesMap = jsonDecode(data);
-      upcomingMoviesMap['results'].removeWhere((e) => e['poster_path'] == null);
-      return searchedMovies = RecommendedMovies.fromJson(upcomingMoviesMap);
+      Map<String, dynamic> searchMoviesMap = jsonDecode(data);
+      searchMoviesMap['results'].removeWhere((e) => e['poster_path'] == null);
+      setState(() {
+        searchedMovies = RecommendedMovies.fromJson(searchMoviesMap);
+      });
+      for (int i = 0; i < (searchedMovies?.results?.length ?? 0); i++) {
+        _moviesFavStatus[searchedMovies?.results?[i].id ?? 0] = await _checkIfLiked(searchedMovies?.results?[i].id ?? 0);
+      }
+      //return searchedMovies = RecommendedMovies.fromJson(searchMoviesMap);
 
     } else {
       // ignore: avoid_print
@@ -44,7 +50,8 @@ class _SearchState extends State<Search> {
     }
   }
 
-  _checkIfLiked(int id) async {
+  Future<bool> _checkIfLiked(int id) async {
+    bool liked = false;
     if (user != null) {
       await FirebaseFirestore.instance
           .collection('users')
@@ -52,22 +59,27 @@ class _SearchState extends State<Search> {
           .get()
           .then((value) {
         if (value['fav_movies'].contains(id)) {
-          setState(() {
-            _isLiked = true;
-          });
-        } else {
-          setState(() {
-            _isLiked = false;
-          });
+          liked = true;
         }
       });
     }
+    return liked;
   }
 
-  _toggleLike() {
-    setState(() {
-      _isLiked = !_isLiked;
-    });
+  _toggleLike(int id) {
+    if (user != null && _moviesFavStatus[id] != null) {
+      if (_moviesFavStatus[id] == true) {
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .update({'fav_movies': FieldValue.arrayUnion([id])});
+      } else {
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(user!.uid)
+            .update({'fav_movies': FieldValue.arrayRemove([id])});
+      }
+    }
   }
 
   saveFavId(int id) {
@@ -121,30 +133,39 @@ class _SearchState extends State<Search> {
               Padding(
                 padding: const EdgeInsets.only(left: 20, right: 20),
                 child: Container(
-                  height: 50,
+                  height: 55,
                   decoration: BoxDecoration(
                     color: const Color.fromRGBO(36, 37, 41, 1),
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(14),
                   ),
+                  padding: const EdgeInsets.only(top: 3, right: 10),
                   child: TextField(
                     controller: searchController,
                     onChanged: (value) {
                       setState(() {
-                        getUpcomingMovies(value);
+                        getSearchedMovies(value);
                       });
                     },
                     style: const TextStyle(color: Colors.white),
                     decoration: const InputDecoration(
                       border: InputBorder.none,
-                      prefixIcon: Icon(
-                        Icons.search,
-                        color: Colors.white,
+                      prefixIcon: Padding(
+                        padding: EdgeInsets.only(left: 16, right: 11),
+                        child: ImageIcon(
+                          AssetImage('assets/images/searchTextFieldIcon.png'),
+                          color: Color.fromRGBO(202, 202, 202, 1),
+                        ),
+                      ),
+                      prefixIconConstraints: BoxConstraints(
+                        minWidth: 10,
+                        minHeight: 10,
                       ),
                       hintText: 'Search for a movie',
                       hintStyle: TextStyle(
-                        color: Colors.white,
+                        color: Color.fromRGBO(202, 202, 202, 1),
                         fontSize: 15,
                         fontFamily: 'Inter',
+                        fontWeight: FontWeight.w400,
                       ),
                     ),
                   ),
@@ -171,92 +192,93 @@ class _SearchState extends State<Search> {
                       physics: const NeverScrollableScrollPhysics(),
                       itemCount: searchedMovies?.results?.length ?? 0,
                       itemBuilder: (context, index) {
+                        _checkIfLiked(searchedMovies?.results?[index].id ?? 0).then((val) => _moviesFavStatus[searchedMovies?.results?[index].id ?? 0] = val);
                         return Padding(
                           padding: const EdgeInsets.only(left: 20, right: 20),
-                          child: Container(
-                            margin: const EdgeInsets.only(top: 15),
-                            height: 150,
-                            decoration: BoxDecoration(
-                              color: const Color.fromRGBO(36, 37, 41, 1),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Row(
-                              children: [
-                                ClipRRect(
-                                  borderRadius: BorderRadius.circular(15),
-                                  child: Image.network(
-                                    'https://image.tmdb.org/t/p/w500${searchedMovies?.results?[index].posterPath}',
-                                    height: 150,
-                                    width: 100,
-                                    fit: BoxFit.cover,
-                                    filterQuality: FilterQuality.none,
+                          child: GestureDetector(
+                            onTap: () {
+                              pushToMovieDetailsPage(searchedMovies!.results![index].id ?? 0);
+                            },
+                            child: Container(
+                              margin: const EdgeInsets.only(top: 15),
+                              height: 150,
+                              decoration: BoxDecoration(
+                                color: const Color.fromRGBO(36, 37, 41, 1),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Row(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: BorderRadius.circular(15),
+                                    child: Image.network(
+                                      'https://image.tmdb.org/t/p/w500${searchedMovies?.results?[index].posterPath}',
+                                      height: 150,
+                                      width: 100,
+                                      fit: BoxFit.cover,
+                                      filterQuality: FilterQuality.none,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(width: 10),
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width - 170,
-                                      child: Text(
-                                        searchedMovies?.results?[index].title ?? '',
-                                        style: const TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 17,
-                                          fontFamily: 'Inter',
-                                          fontWeight: FontWeight.w500,
+                                  const SizedBox(width: 10),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      SizedBox(
+                                        width: MediaQuery.of(context).size.width - 170,
+                                        child: Text(
+                                          searchedMovies?.results?[index].title ?? '',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 17,
+                                            fontFamily: 'Inter',
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                          softWrap: true,
+                                          overflow: TextOverflow.clip,
+                                          maxLines: 3,
                                         ),
-                                        softWrap: true,
-                                        overflow: TextOverflow.clip,
                                       ),
-                                    ),
-                                    const SizedBox(height: 10),
-                                    Text(
-                                      searchedMovies?.results?[index].releaseDate?.substring(0, 4) ?? '',
-                                      style: const TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 15,
-                                        fontFamily: 'Inter',
+                                      const SizedBox(height: 10),
+                                      Text(
+                                        searchedMovies?.results?[index].releaseDate ?? '',
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 15,
+                                            fontFamily: 'Inter',
+                                          ),
                                       ),
-                                    ),
-                                    FloatingActionButton(
-                                      onPressed: () async {
-                                        await _checkIfLiked(searchedMovies?.results?[index].id ?? 0);
-                                        await _toggleLike();
-                                        if (_isLiked) {
-                                          saveFavId(searchedMovies?.results?[index].id ?? 0);
-                                        } else {
-                                          deleteFavId(searchedMovies?.results?[index].id ?? 0);
-                                        }
-                                      },
-                                      backgroundColor: Colors.transparent,
-                                      elevation: 0,
-                                      child: _isLiked
-                                          ? Image.asset(
-                                        'assets/images/FavDetailsOrange.png',
-                                        height: 30,
-                                        width: 30,
-                                      ) : Image.asset(
-                                        'assets/images/favDetails.png',
-                                        height: 30,
-                                        width: 30,
+                                      FloatingActionButton(
+                                        onPressed: () async {
+                                          _moviesFavStatus[searchedMovies?.results?[index].id ?? 0] = !_moviesFavStatus[searchedMovies?.results?[index].id ?? 0]!;
+                                          await _toggleLike(searchedMovies?.results?[index].id ?? 0);
+                                          setState(() {});
+                                        },
+                                        backgroundColor: Colors.transparent,
+                                        elevation: 0,
+                                        child: _moviesFavStatus[searchedMovies?.results?[index].id ?? 0] ?? false
+                                            ? Image.asset(
+                                          'assets/images/FavDetailsOrange.png',
+                                          height: 30,
+                                          width: 30,
+                                        ) : Image.asset(
+                                          'assets/images/favDetails.png',
+                                          height: 30,
+                                          width: 30,
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
+                                    ],
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         );
                       },
                     ),
-
             ],
           ),
         ),
       ),
-
     );
   }
 }
